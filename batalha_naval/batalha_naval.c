5f5e54893efb48f7e4c558155f2cef1b442cd99d
espachito <elder@suportis.com.br>
2026-02-26 17:19:01 +0000

    Commit inicial

---
+++ batalha_naval/batalha_naval.c
@@ -0,0 +1,31 @@
+/*
+
+DESAFIO 3 - TEMA 5 - BATALHA NAVAL (Nível NOVATO)
+
+Posicionando Navios no Tabuleiro
+
+Neste primeiro desafio, este código utiliza vetores (arrays unidimensionais) em C para representar um tabuleiro simplificado e posicionar dois navios nele: um na vertical e
+outro na horizontal.
+
+Autor: Elder de Souza Pachito - Matrícula: 202505123303
+
+*/
+
+// Inclusão da biblioteca necessária para a entrada e exibição de dados:
+#include <stdio.h>
+
+int main() {
+
+
+
+
+
+
+    
+   
+    // Salta linha(s) na tela do terminal:
+    printf("\n\n");
+    
+    // Finalização do bloco principal do código:
+    return 0;
+}
