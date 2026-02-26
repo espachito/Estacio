7a2ce4e5e9bff1989b65eddb84bdbcd47160f9a8
espachito <elder@suportis.com.br>
2026-02-26 20:51:13 +0000

    Versão nível AVENTUREIRO do desafio BATALHA NAVAL do Tema 5

--- batalha_naval/batalha_naval.c
+++ batalha_naval/batalha_naval.c
@@ -1,11 +1,10 @@
 /*
 
-DESAFIO 3 - TEMA 5 - BATALHA NAVAL (Nível NOVATO)
+DESAFIO 3 - TEMA 5 - BATALHA NAVAL (Nível AVENTUREIRO)
 
-Posicionando Navios no Tabuleiro
+Tabuleiro Completo e Navios Diagonais
 
-Neste primeiro desafio, este código utiliza vetores (arrays unidimensionais) em C para representar um tabuleiro simplificado e posicionar dois navios nele: um na vertical e
-outro na horizontal.
+Nesta etapa, aprimoramos o jogo de Batalha Naval adicionando a complexidade de navios posicionados na diagonal.
 
 Autor: Elder de Souza Pachito - Matrícula: 202505123303
 
@@ -13,41 +12,33 @@ Autor: Elder de Souza Pachito - Matrícula: 202505123303
 
 // Inclusão da biblioteca necessária para a entrada e exibição de dados:
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+// Criando a matriz principal que representará o tabuleiro virtual do jogo em memória, com ouma variável global estática:
+static int tabuleiro[10][10];
 
-    // Criando a matriz principal que representará o tabuleiro virtual do jogo em memória:
-    int tabuleiro[10][10];
-
-    // Criando os vetores que representarão os navios:
-    int nav1[3] = {3,3,3};            // Navio 1 tem 3 posições de tamanho, conforme sugerido
-    int nav2[3] = {3,3,3};            // Navio 2 tem 3 posições de tamanho, conforme sugerido
-    int tamanho;                      // Variável para armazenar o tamanho de um navio
-    int posicao_i, posicao_j;         // Variáveis auxiliares para posicionar uma parte do corpo de um navio nas coordenadas i e j
-    int inc_i, inc_j;                 // Variáveis auxiliares para direcionar o posicionamento de uma parte do corpo de um navio através de incrementos em i e em j
-
-    // Definindo as posições iniciais dos navios, através das coordenadas (i, j) de uma de suas extremidades, e da direção do seu posicionamento a partir de (i. j):
-    // Legenda para a direção de um navio:  0 = Leste            3 = Noroeste            6 = Sul
-    //                                      1 = Nordeste         4 = Oeste               7 = Sudeste
-    //                                      2 = Norte            5 = Sudoeste
-    
-    int pos_nav1[3] = {3, 2, 0};  // i = 3; j = 2; e a direção = 0 (Leste)
-    int pos_nav2[3] = {7, 6, 1};  // i = 7; j = 6; e a direção = 1 (Nordeste)
-
-    // Inicializando o tabuleiro com água (0) em todas as suas posições:
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
-        }
-    }
-
-    // Colocando o Navio 1 no tabuleiro:
-    tamanho = (int)(sizeof(nav1)/sizeof(nav1[0]));      // Obtém o tamanho do Navio 1
-    posicao_i = pos_nav1[0];
-    posicao_j = pos_nav1[1];
+// Procedimento que recebe o vetor NAVIO e o vetor POSICAO e o insere no tabuleiro:
+void insere_navio(int * navio, int * posicao, int tamanho) {
+    int posicao_i, posicao_j;                           // Variáveis auxiliares para posicionar uma parte do corpo de um navio nas coordenadas i e j
+    int inc_i, inc_j;                                   // Variáveis auxiliares para direcionar o posicionamento de uma parte do corpo do navio através de incrementos em i e em j
+    // Colocando o navio no tabuleiro:
+    posicao_i = posicao[0];
+    posicao_j = posicao[1];
     for (int i = 0; i < tamanho; i++) {
-        tabuleiro[posicao_i][posicao_j] = nav1[i];
-        switch (pos_nav1[2]) {
+        if ((posicao_i >=0) && (posicao_i < 10) && (posicao_j >= 0) && (posicao_j < 10)) {
+            if (tabuleiro[posicao_i][posicao_j] == 0) {
+                tabuleiro[posicao_i][posicao_j] = navio[i];
+            } else {
+                printf("\n\n*** ERRO! - Posição de navio se sobrepões a navio existente.\n\n");      // Em caso de sobreposição, exibe mensagem de erro e aborta a execução do programa
+                abort();
+            }
+        } else {
+            printf("\n\n*** ERRO! - Posição de navio ultrapassa o tabuleiro.\n\n");                 // Em caso de posição fora dos limites do tabuleiro, exibe mensagem de erro e aborta a execução do programa
+            abort();
+        }
+        
+        tabuleiro[posicao_i][posicao_j] = navio[i];
+        switch (posicao[2]) {
             case 0:                                     // Caso o navio esteja direcionado para o Leste
                 inc_i = 0;                              
                 inc_j = 1;
@@ -81,59 +72,47 @@ int main() {
                 inc_j = 1;
                 break;
             default:
-                printf("\n\n*** ERRO! - Direção inválida para o Navio 1\n\n");      // Em caso de direção inválida, exibe mensagem de erro e aborta a execução do programa
-                return 0;
+                printf("\n\n*** ERRO! - Direção inválida para um navio.\n\n");      // Em caso de direção inválida, exibe mensagem de erro e aborta a execução do programa
+                abort();
         }
         posicao_i += inc_i;         // Aponta para a próxima posição a ser ocupada pelo Navio 1, conforme os incrementos/decrementos aplicados em i e j
         posicao_j += inc_j;
+    }
+}
+
+int main() {
+
+    // Criando os vetores que representarão os navios:
+    int nav1[3] = {3,3,3};            // Navio 1 tem 3 posições de tamanho;
+    int nav2[3] = {3,3,3};            // Navio 2 tem 3 posições de tamanho;
+    int nav3[4] = {3,3,3,3};          // Mavio 3 tem 4 posições de tamanho;
+    int nav4[2] = {3,3};              // Navio 4 tem 2 posições de tamanha;
+    int tamanho;                      // Variável para armazenar o tamanho de um navio
+    int posicao_i, posicao_j;         // Variáveis auxiliares para posicionar uma parte do corpo de um navio nas coordenadas i e j
+    int inc_i, inc_j;                 // Variáveis auxiliares para direcionar o posicionamento de uma parte do corpo de um navio através de incrementos em i e em j
+
+    // Definindo as posições iniciais dos navios, através das coordenadas (i, j) de uma de suas extremidades, e da direção do seu posicionamento a partir de (i. j):
+    // Legenda para a direção de um navio:  0 = Leste            3 = Noroeste            6 = Sul
+    //                                      1 = Nordeste         4 = Oeste               7 = Sudeste
+    //                                      2 = Norte            5 = Sudoeste
+    
+    int pos_nav1[3] = {3, 2, 0};  // i = 3; j = 2; e a direção = 0 (Leste)
+    int pos_nav2[3] = {7, 6, 1};  // i = 7; j = 6; e a direção = 1 (Nordeste)
+    int pos_nav3[3] = {9, 4, 3};  // i = 9, j = 1; e a direção = 2 (Noroeste)
+    int pos_nav4[3] = {1, 8, 6};  // i = 1, j = 8; e a direção = 4 (Sul)
+
+    // Inicializando o tabuleiro com água (0) em todas as suas posições:
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
     }
 
-    // Colocando o Navio 2 no tabuleiro:
-    tamanho = (int)(sizeof(nav2)/sizeof(nav2[0]));      // Obtém o tamanho do Navio 2
-    posicao_i = pos_nav2[0];
-    posicao_j = pos_nav2[1];
-    for (int i = 0; i < tamanho; i++) {
-        tabuleiro[posicao_i][posicao_j] = nav2[i];
-        switch (pos_nav2[2]) {
-            case 0:                                     // Caso o navio esteja direcionado para o Leste
-                inc_i = 0;
-                inc_j = 1;
-                break;
-            case 1:                                     // Caso o navio esteja direcionado para o Nordeste
-                inc_i = -1;
-                inc_j = 1;
-                break;
-            case 2:                                     // Caso o navio esteja direcionado para o Norte
-                inc_i = -1;
-                inc_j = 0;
-                break;
-            case 3:                                     // Caso o navio esteja direcionado para o Noroeste
-                inc_i = -1;
-                inc_j = -1;
-                break;
-            case 4:                                     // Caso o navio esteja direcionado para o Oeste
-                inc_i = 0;
-                inc_j = -1;
-                break;
-            case 5:                                     // Caso o navio esteja direcionado para o Sudoeste
-                inc_i = 1;
-                inc_j = -1;
-                break;
-            case 6:                                     // Caso o navio esteja direcionado para o Sul
-                inc_i = 1;
-                inc_j = 0;
-                break;
-            case 7:                                     // Caso o navio esteja direcionado para o Sudeste
-                inc_i = 1;
-                inc_j = 1;
-                break;
-            default:
-                printf("\n\n*** ERRO! - Direção inválida para o Navio 2\n\n");      // Em caso de direção inválida, exibe mensagem de erro e aborta a execução do programa
-                return 0;
-        }
-        posicao_i += inc_i;         // Aponta para a próxima posição a ser ocupada pelo Navio 1, conforme os incrementos/decrementos aplicados em i e j
-        posicao_j += inc_j;
-    }
+    // Colocando os Navios no tabuleiro:
+    insere_navio(nav1, pos_nav1, 3);
+    insere_navio(nav2, pos_nav2, 3);
+    insere_navio(nav3, pos_nav3, 4);
+    insere_navio(nav4, pos_nav4, 2);
 
     // Exibindo o tabuleiro e seu conteúdi (navios):
     for (int i = 0; i < 10; i++) {
